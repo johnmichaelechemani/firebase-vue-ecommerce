1f7a58fe18fad9996c8ccfbd46554f5df14485d8
Your Name <deudajm@gmail.com>
2024-12-13 21:22:10 +0800

    add search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,9 @@
       <div class="ml-2">
         <h1 class="font-semibold text-lg">Jm's Shops</h1>
       </div>
+      <div class="border border-gray-700/50 py-1.5 px-2 text-sm w-1/3">
+        <input type="search" class="w-full outline-none placeholder:text-sm placeholder:text-gray-700" placeholder="Search.." />
+      </div>
       <div class="flex justify-end items-center gap-2 m-2">
         <div class="size-8 rounded-full bg-gray-500"></div>
         <div class="size-8 rounded-full bg-gray-500"></div>
