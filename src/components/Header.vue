c5307f861bb5136a281c572ae0df9c0d75de8fb6
Your Name <deudajm@gmail.com>
2024-12-14 12:46:08 +0800

    seperate settings vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
 import User from "../assets/dummyImages/user.jpg";
 import { Icon } from "@iconify/vue";
 import { ref } from "vue";
+import Settings from "@/views/Settings.vue";
 
 const isShowSetting = ref(false);
 const showSetting = () => {
@@ -14,6 +15,7 @@ const hideSetting = () => {
 const isLoggedIn = ref(true);
 const logout = () => {
   isLoggedIn.value = false;
+  isShowSetting.value = false;
 };
 </script>
 <template>
@@ -122,58 +124,7 @@ const logout = () => {
       />
     </button>
     <div class="mx-2 mb-2">
-      <p class="text-xs font-semibold text-gray-500">My Account</p>
-      <button class="flex justify-center items-center gap-2 my-2">
-        <div class="border p-1 shadow place-items-center">
-          <Icon
-            icon="material-symbols-light:edit-outline"
-            width="24"
-            height="24"
-          />
-        </div>
-        <p class="text-sm font-semibold text-gray-700">Account</p>
-      </button>
-      <button class="flex justify-center items-center gap-2 my-2">
-        <div class="border p-1 shadow place-items-center">
-          <Icon
-            icon="material-symbols-light:location-on-outline"
-            width="24"
-            height="24"
-          />
-        </div>
-        <p class="text-sm font-semibold text-gray-700">Addresses</p>
-      </button>
-      <p class="text-xs font-semibold text-gray-500">Support</p>
-      <button class="flex justify-center items-center gap-2 my-2">
-        <div class="border p-1 shadow place-items-center">
-          <Icon
-            icon="material-symbols-light:help-outline"
-            width="24"
-            height="24"
-          />
-        </div>
-        <p class="text-sm font-semibold text-gray-700">Help Centre</p>
-      </button>
-      <button class="flex justify-center items-center gap-2 my-2">
-        <div class="border p-1 shadow place-items-center">
-          <Icon
-            icon="material-symbols-light:info-outline"
-            width="24"
-            height="24"
-          />
-        </div>
-        <p class="text-sm font-semibold text-gray-700">About</p>
-      </button>
-      <button class="flex justify-center items-center gap-2 my-2">
-        <div class="border p-1 shadow place-items-center">
-          <Icon
-            icon="fluent:people-community-32-light"
-            width="24"
-            height="24"
-          />
-        </div>
-        <p class="text-sm font-semibold text-gray-700">Community Rules</p>
-      </button>
+      <Settings />
       <button
         @click="logout"
         class="border p-2 my-4 border-gray-700/50 shadow-sm w-full text-gray-800 text-sm font-semibold place-items-center"
---
+++ src/views/Settings.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <p class="text-xs font-semibold text-gray-500">My Account</p>
+  <button class="flex justify-center items-center gap-2 my-2">
+    <div class="border p-1 shadow place-items-center">
+      <Icon icon="material-symbols-light:edit-outline" width="24" height="24" />
+    </div>
+    <p class="text-sm font-semibold text-gray-700">Account</p>
+  </button>
+  <button class="flex justify-center items-center gap-2 my-2">
+    <div class="border p-1 shadow place-items-center">
+      <Icon
+        icon="material-symbols-light:location-on-outline"
+        width="24"
+        height="24"
+      />
+    </div>
+    <p class="text-sm font-semibold text-gray-700">Addresses</p>
+  </button>
+  <p class="text-xs font-semibold text-gray-500">Support</p>
+  <button class="flex justify-center items-center gap-2 my-2">
+    <div class="border p-1 shadow place-items-center">
+      <Icon icon="material-symbols-light:help-outline" width="24" height="24" />
+    </div>
+    <p class="text-sm font-semibold text-gray-700">Help Centre</p>
+  </button>
+  <button class="flex justify-center items-center gap-2 my-2">
+    <div class="border p-1 shadow place-items-center">
+      <Icon icon="material-symbols-light:info-outline" width="24" height="24" />
+    </div>
+    <p class="text-sm font-semibold text-gray-700">About</p>
+  </button>
+  <button class="flex justify-center items-center gap-2 my-2">
+    <div class="border p-1 shadow place-items-center">
+      <Icon icon="fluent:people-community-32-light" width="24" height="24" />
+    </div>
+    <p class="text-sm font-semibold text-gray-700">Community Rules</p>
+  </button>
+</template>
