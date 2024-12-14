3bb64fecac40a06d9d69d4b8e665b737407234ec
Your Name <deudajm@gmail.com>
2024-12-14 18:57:37 +0800

    new updates

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,6 +7,7 @@
         <img
           :src="Shop"
           alt=""
+          loading="lazy"
           class="w-full h-full object-center object-cover"
         />
       </div>
--- src/views/Purchase.vue
+++ src/views/Purchase.vue
@@ -1,9 +1,56 @@
-<script setup></script>
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
 
 <template>
   <div
     class="sm:ml-72 ml-20 fixed top-14 left-0 sm:w-[calc(100%-18rem)] w-[calc(100%-5rem)] h-full"
   >
-    <div class="p-2">Purchase view</div>
+    <div class="p-2">
+      <p class="text-sm font-semibold">My Purchase</p>
+      <div class="my-4 flex flex-wrap items-center gap-4">
+        <div>
+          <div class="p-2 bg-gray-800 text-white">
+            <Icon
+              icon="material-symbols-light:credit-card-outline"
+              width="24"
+              height="24"
+            />
+          </div>
+          <p class="text-sm font-medium">To Pay</p>
+        </div>
+
+        <div>
+          <div class="p-2 bg-gray-800 text-white">
+            <Icon
+              icon="material-symbols-light:credit-card-outline"
+              width="24"
+              height="24"
+            />
+          </div>
+          <p class="text-sm font-medium">To Ship</p>
+        </div>
+        <div>
+          <div class="p-2 bg-gray-800 text-white">
+            <Icon
+              icon="material-symbols-light:credit-card-outline"
+              width="24"
+              height="24"
+            />
+          </div>
+          <p class="text-sm font-medium">To Recieve</p>
+        </div>
+        <div>
+          <div class="p-2 bg-gray-800 text-white">
+            <Icon
+              icon="material-symbols-light:credit-card-outline"
+              width="24"
+              height="24"
+            />
+          </div>
+          <p class="text-sm font-medium">To Rate</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
