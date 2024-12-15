d71a42a219236e56a8caf577c6ce5d4dec9f4606
Your Name <deudajm@gmail.com>
2024-12-15 10:17:03 +0800

    `feat: updated Purchase.vue with new purchase products and button config`

--- src/views/Purchase.vue
+++ src/views/Purchase.vue
@@ -26,7 +26,7 @@ const purchaseProducts = ref([
     sold: Math.floor(Math.random() * 1000) + 100,
   },
   {
-    id: 2,
+    id: 3,
     name: "Shoes ni Michael",
     store: "Mike Store",
     quantity: Math.floor(Math.random() * 5) + 1,
@@ -39,7 +39,7 @@ const purchaseProducts = ref([
     sold: Math.floor(Math.random() * 1000) + 100,
   },
   {
-    id: 2,
+    id: 4,
     name: "Shoes ni Mic",
     store: "Mike Store",
     quantity: Math.floor(Math.random() * 5) + 1,
@@ -52,12 +52,25 @@ const purchaseProducts = ref([
     sold: Math.floor(Math.random() * 1000) + 100,
   },
   {
-    id: 2,
+    id: 5,
     name: "Shoes ni John",
     store: "Mike Store",
     quantity: Math.floor(Math.random() * 5) + 1,
     category: "shoes",
     status: "rate",
+    price: Math.floor(Math.random() * 500) + 50,
+    image: Shoes,
+    ratings: Math.floor(Math.random() * 5) + 1,
+    discount: Math.floor(Math.random() * 50),
+    sold: Math.floor(Math.random() * 1000) + 100,
+  },
+  {
+    id: 6,
+    name: "Shoes ni John",
+    store: "Mike Store",
+    quantity: Math.floor(Math.random() * 5) + 1,
+    category: "shoes",
+    status: "completed",
     price: Math.floor(Math.random() * 500) + 50,
     image: Shoes,
     ratings: Math.floor(Math.random() * 5) + 1,
@@ -71,7 +84,7 @@ const purchasesStatus = ref([
     id: 1,
     name: "To Pay",
     status: "pay",
-    icon: "material-symbols-light:credit-card-outline",
+    icon: "mdi:credit-card-chip-outline",
   },
   {
     id: 2,
@@ -112,6 +125,28 @@ const filteredStats = computed(() => {
   }
   return purchaseProducts.value;
 });
+const getButtonConfig = (status) => {
+  const buttonMap = {
+    completed: {
+      text: "Buy Again",
+      action: () => console.log("Buy Again Clicked"),
+    },
+    pay: { text: "Cancel", action: () => console.log("Cancel Clicked") },
+    ship: {
+      text: "Track Order",
+      action: () => console.log("Track Order Clicked"),
+    },
+    rate: {
+      text: "Rate Order",
+      action: () => console.log("Rate Order Clicked"),
+    },
+    recieve: {
+      text: "View Order",
+      action: () => console.log("View Order Clicked"),
+    },
+  };
+  return buttonMap[status] || null;
+};
 </script>
 
 <template>
@@ -137,72 +172,84 @@ const filteredStats = computed(() => {
           >
             <Icon :icon="item.icon" width="24" height="24" />
           </div>
-          <p class="text-sm my-2 text-center font-semibold">{{ item.name }}</p>
+          <p class="text-sm mt-2 text-center font-semibold">{{ item.name }}</p>
         </div>
       </div>
     </div>
     <p class="text-sm font-semibold p-2 capitalize">
       {{ Object.keys($route.query).length === 0 ? "All" : route.query.status }}
     </p>
-    <div class="mx-2 flex flex-wrap gap-2">
-      <div
-        v-if="filteredStats"
-        v-for="item in filteredStats"
-        :key="item.id"
-        class="shadow-sm border p-2 w-full sm:max-w-72"
-      >
-        <div class="flex justify-between items-center">
-          <div class="text-sm font-semibold flex justify-between items-center">
-            <span
-              ><Icon
-                icon="material-symbols-light:store"
-                width="24"
-                height="24" /></span
-            >{{ item.store }}
+    <div class="overflow-y-scroll no-scrollbar h-[calc(100vh-3.5rem)] pb-48">
+      <div class="mx-2 flex flex-wrap gap-2">
+        <div
+          v-if="filteredStats"
+          v-for="item in filteredStats"
+          :key="item.id"
+          class="shadow-sm border p-2 w-full sm:max-w-72"
+        >
+          <div class="flex justify-between items-center">
+            <div
+              class="text-sm font-semibold flex justify-between items-center"
+            >
+              <span
+                ><Icon
+                  icon="material-symbols-light:store"
+                  width="24"
+                  height="24" /></span
+              >{{ item.store }}
+            </div>
+            <div class="text-xs shadow-sm font-medium border px-2 capitalize">
+              {{ item.status }}
+            </div>
           </div>
-          <div class="text-xs shadow-sm font-medium border px-2 capitalize">
-            To {{ item.status }}
+
+          <div class="flex justify-start items-start gap-2">
+            <div
+              class="sm:size-20 size-10 bg-gray-700/10 border-gray-700/20 border relative"
+            >
+              <img
+                :src="item.image"
+                loading="lazy"
+                alt=""
+                class="object-center h-full w-full object-cover"
+              />
+            </div>
+            <div>
+              <p class="text-sm font-semibold">{{ item.name }}</p>
+              <div class="flex justify-start items-center gap-2">
+                <p class="text-xs font-medium">${{ item.price }}</p>
+              </div>
+
+              <div class="font-semibold flex justify-start items-center gap-2">
+                <span class="font-semibold text-xs">x{{ item.quantity }} </span>
+              </div>
+              <div class="font-semibold flex justify-start items-center gap-2">
+                <span class="font-semibold text-xs"
+                  >Total {{ item.quantity }} Item :
+                  <span class="text-sm font-bold"
+                    >${{ item.quantity * item.price }}</span
+                  >
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-end" v-if="getButtonConfig(item.status)">
+            <button
+              class="border text-sm border-gray-700/50 font-semibold px-4 py-2"
+              @click="getButtonConfig(item.status).action"
+            >
+              {{ getButtonConfig(item.status).text }}
+            </button>
           </div>
         </div>
-
-        <div class="flex justify-start items-start gap-2">
-          <div
-            class="sm:size-20 size-10 bg-gray-700/10 border-gray-700/20 border relative"
-          >
-            <img
-              :src="item.image"
-              loading="lazy"
-              alt=""
-              class="object-center h-full w-full object-cover"
-            />
-          </div>
-          <div>
-            <p class="text-sm font-semibold">{{ item.name }}</p>
-            <div class="flex justify-start items-center gap-2">
-              <p class="text-xs font-medium">${{ item.price }}</p>
-            </div>
-
-            <div class="font-semibold flex justify-start items-center gap-2">
-              <span class="font-semibold text-xs">x{{ item.quantity }} </span>
-            </div>
-            <div class="font-semibold flex justify-start items-center gap-2">
-              <span class="font-semibold text-xs"
-                >Total {{ item.quantity }} Item :
-                <span class="text-sm font-bold"
-                  >${{ item.quantity * item.price }}</span
-                >
-              </span>
-            </div>
-          </div>
+        <div
+          v-if="filteredStats.length === 0"
+          class="text-sm font-semibold flex justify-start items-center gap-2"
+        >
+          <span class="p-2 border shadow-sm"
+            ><Icon icon="mdi:cup-empty" width="24" height="24" /></span
+          >No Orders Yet
         </div>
-      </div>
-      <div
-        v-if="filteredStats.length === 0"
-        class="text-sm font-semibold flex justify-start items-center gap-2"
-      >
-        <span class="p-2 border shadow-sm"
-          ><Icon icon="mdi:cup-empty" width="24" height="24" /></span
-        >No Orders Yet
       </div>
     </div>
   </div>
