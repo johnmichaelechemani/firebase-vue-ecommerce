afe3f51e4a371fe0032046d11d008b57381c2575
Your Name <deudajm@gmail.com>
2024-12-15 09:36:28 +0800

    `Added new purchase products and status options to Purchase.vue`

--- src/views/Purchase.vue
+++ src/views/Purchase.vue
@@ -25,6 +25,45 @@ const purchaseProducts = ref([
     discount: Math.floor(Math.random() * 50),
     sold: Math.floor(Math.random() * 1000) + 100,
   },
+  {
+    id: 2,
+    name: "Shoes ni Michael",
+    store: "Mike Store",
+    quantity: Math.floor(Math.random() * 5) + 1,
+    category: "shoes",
+    status: "ship",
+    price: Math.floor(Math.random() * 500) + 50,
+    image: Shoes,
+    ratings: Math.floor(Math.random() * 5) + 1,
+    discount: Math.floor(Math.random() * 50),
+    sold: Math.floor(Math.random() * 1000) + 100,
+  },
+  {
+    id: 2,
+    name: "Shoes ni Mic",
+    store: "Mike Store",
+    quantity: Math.floor(Math.random() * 5) + 1,
+    category: "shoes",
+    status: "recieve",
+    price: Math.floor(Math.random() * 500) + 50,
+    image: Shoes,
+    ratings: Math.floor(Math.random() * 5) + 1,
+    discount: Math.floor(Math.random() * 50),
+    sold: Math.floor(Math.random() * 1000) + 100,
+  },
+  {
+    id: 2,
+    name: "Shoes ni John",
+    store: "Mike Store",
+    quantity: Math.floor(Math.random() * 5) + 1,
+    category: "shoes",
+    status: "rate",
+    price: Math.floor(Math.random() * 500) + 50,
+    image: Shoes,
+    ratings: Math.floor(Math.random() * 5) + 1,
+    discount: Math.floor(Math.random() * 50),
+    sold: Math.floor(Math.random() * 1000) + 100,
+  },
 ]);
 
 const purchasesStatus = ref([
@@ -51,6 +90,18 @@ const purchasesStatus = ref([
     name: "To Rate",
     status: "rate",
     icon: "mdi:star-outline",
+  },
+  {
+    id: 5,
+    name: "Completed",
+    status: "completed",
+    icon: "mdi:check",
+  },
+  {
+    id: 6,
+    name: "Cancelled",
+    status: "cancelled",
+    icon: "mdi:cancel-box-outline",
   },
 ]);
 
@@ -90,14 +141,17 @@ const filteredStats = computed(() => {
         </div>
       </div>
     </div>
-
-    <div class="mx-2 flex flex-wrap">
+    <p class="text-sm font-semibold p-2 capitalize">
+      {{ Object.keys($route.query).length === 0 ? "All" : route.query.status }}
+    </p>
+    <div class="mx-2 flex flex-wrap gap-2">
       <div
+        v-if="filteredStats"
         v-for="item in filteredStats"
         :key="item.id"
-        class="shadow-sm p-2 max-w-72"
+        class="shadow-sm border p-2 w-full sm:max-w-72"
       >
-        <div class="text-sm font-semibold flex justify-between items-center">
+        <div class="flex justify-between items-center">
           <div class="text-sm font-semibold flex justify-between items-center">
             <span
               ><Icon
@@ -113,7 +167,7 @@ const filteredStats = computed(() => {
 
         <div class="flex justify-start items-start gap-2">
           <div
-            class="size-20 bg-gray-700/10 border-gray-700/20 border relative"
+            class="sm:size-20 size-10 bg-gray-700/10 border-gray-700/20 border relative"
           >
             <img
               :src="item.image"
@@ -125,7 +179,7 @@ const filteredStats = computed(() => {
           <div>
             <p class="text-sm font-semibold">{{ item.name }}</p>
             <div class="flex justify-start items-center gap-2">
-              <p class="text-sm font-bold">${{ item.price }}</p>
+              <p class="text-xs font-medium">${{ item.price }}</p>
             </div>
 
             <div class="font-semibold flex justify-start items-center gap-2">
@@ -133,12 +187,22 @@ const filteredStats = computed(() => {
             </div>
             <div class="font-semibold flex justify-start items-center gap-2">
               <span class="font-semibold text-xs"
-                >Total {{ item.quantity }} Item : $
-                {{ item.quantity * item.price }}
+                >Total {{ item.quantity }} Item :
+                <span class="text-sm font-bold"
+                  >${{ item.quantity * item.price }}</span
+                >
               </span>
             </div>
           </div>
         </div>
+      </div>
+      <div
+        v-if="filteredStats.length === 0"
+        class="text-sm font-semibold flex justify-start items-center gap-2"
+      >
+        <span class="p-2 border shadow"
+          ><Icon icon="mdi:cup-empty" width="24" height="24" /></span
+        >No Orders Yet
       </div>
     </div>
   </div>
