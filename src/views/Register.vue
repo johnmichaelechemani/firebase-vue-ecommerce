80c84b674851f53378462705aad72dc1bda298bb
Your Name <deudajm@gmail.com>
2024-12-20 15:34:42 +0800

    `Added account type selection to registration form`

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,25 +8,48 @@
       <div class="border p-5">
         <h1 class="text-lg font-bold my-3">Sign Up</h1>
         <div class="my-2">
-          <p class="text-sm font-semibold text-gray-700">Name</p>
+          <p class="text-sm font-semibold text-gray-700">
+            Name:<span title="important">*</span>
+          </p>
           <input
             type="text"
             class="outline-none border p-2 min-w-52 sm:min-w-80"
           />
         </div>
         <div class="my-2">
-          <p class="text-sm font-semibold text-gray-700">Email</p>
+          <p class="text-sm font-semibold text-gray-700">
+            Email:<span title="important">*</span>
+          </p>
           <input
             type="email"
             class="outline-none border p-2 min-w-52 sm:min-w-80"
           />
         </div>
         <div class="my-2">
-          <p class="text-sm font-semibold text-gray-700">Password</p>
+          <p class="text-sm font-semibold text-gray-700">
+            Password: <span title="important">*</span>
+          </p>
           <input
             type="password"
             class="outline-none border p-2 min-w-52 sm:min-w-80"
           />
+        </div>
+        <div class="my-2">
+          <p class="text-sm font-semibold text-gray-700">
+            Select Account Type: <span title="important">*</span>
+          </p>
+          <select
+            name=""
+            id=""
+            class="border w-full text-sm font-semibold text-gray-700 py-2 my-1"
+          >
+            <option selected hidden>Choose a type</option>
+            <option value="customer" class="text-xs font-semibold">
+              Customer
+            </option>
+            <option value="seller" class="text-xs font-semibold">Seller</option>
+            <option value="rider" class="text-xs font-semibold">Rider</option>
+          </select>
         </div>
 
         <div class="mt-4">
