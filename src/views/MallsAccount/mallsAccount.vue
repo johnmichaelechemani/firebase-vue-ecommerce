3b84eef3c71c21d2a9252aeb2be09acbbcac5640
Your Name <deudajm@gmail.com>
2024-12-20 12:32:33 +0800

    new updates

--- src/views/MallsAccount/mallsAccount.vue
+++ src/views/MallsAccount/mallsAccount.vue
@@ -1,6 +1,8 @@
 <script setup>
 import ProductCard from "@/components/ProductCard.vue";
 import { products } from "@/store";
+import { ref } from "vue";
+const activeTab = ref("shop");
 </script>
 
 <template>
@@ -15,17 +17,48 @@ import { products } from "@/store";
       <h1 class="font-semibold text-lg">Store ni Mike</h1>
     </div>
     <div class="flex justify-start items-center gap-4">
-      <button class="px-4 bg-gray-800 py-1 text-sm font-semibold text-white">
+      <button
+        class="px-4 py-1 text-sm font-semibold"
+        :class="{
+          'bg-gray-800 text-white': activeTab === 'shop',
+          'text-gray-800': activeTab !== 'shop',
+        }"
+        @click="activeTab = 'shop'"
+      >
         Shop
       </button>
-      <button class="text-sm px-4 py-1 font-semibold">Products</button>
-      <button class="text-sm px-4 py-1 font-semibold">Categories</button>
+      <button
+        class="px-4 py-1 text-sm font-semibold"
+        :class="{
+          'bg-gray-800 text-white': activeTab === 'products',
+          'text-gray-800': activeTab !== 'products',
+        }"
+        @click="activeTab = 'products'"
+      >
+        Products
+      </button>
+      <button
+        class="px-4 py-1 text-sm font-semibold"
+        :class="{
+          'bg-gray-800 text-white': activeTab === 'categories',
+          'text-gray-800': activeTab !== 'categories',
+        }"
+        @click="activeTab = 'categories'"
+      >
+        Categories
+      </button>
     </div>
     <hr />
     <div class="my-2">
       <p class="font-semibold text-sm my-2">Recommended For You</p>
       <div class="flex flex-wrap gap-1">
-        <ProductCard :products="products" />
+        <ProductCard v-if="activeTab === 'shop'" :products="products" />
+      </div>
+      <div v-if="activeTab === 'products'">
+        <p>Products content goes here.</p>
+      </div>
+      <div v-if="activeTab === 'categories'">
+        <p>Categories content goes here.</p>
       </div>
     </div>
   </div>
