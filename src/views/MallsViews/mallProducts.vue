871426d6a13def988818a2c40194aa2451656e04
Your Name <deudajm@gmail.com>
2024-12-24 14:33:23 +0800

    `feat: Added product category field and updated form to include it`

--- src/views/MallsViews/mallProducts.vue
+++ src/views/MallsViews/mallProducts.vue
@@ -1,10 +1,21 @@
 <script setup>
 import { ref } from "vue";
+import { useAuth } from "@/firebase.auth";
+import { userData } from "@/store";
+import {
+  collection,
+  addDoc,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
+const { user } = useAuth();
+const firestore = getFirestore();
 const productName = ref("");
 const productDiscription = ref("");
 const productPrice = ref("");
 const productDiscount = ref("");
-const productImage = ref(null);
+const productImage = ref(" https://via.placeholder.com/150?text=Nothing");
+const productCategory = ref("");
 
 const clear = () => {
   productName.value = "";
@@ -12,17 +23,36 @@ const clear = () => {
   productPrice.value = "";
   productDiscount.value = "";
   productImage.value = null;
+  productCategory.value = "";
 };
 const add = async () => {
   try {
+    await addDoc(collection(firestore, "products"), {
+      name: productName.value,
+      discription: productDiscription.value,
+      price: productPrice.value,
+      image: productImage.value,
+      discount: productDiscount.value,
+      mall: true,
+      category: productCategory.value,
+      mallId: user.value.uid,
+      mallName: userData.value.userName,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("Adding Success");
   } catch (e) {
     console.log("Error", e);
   }
+
   console.log(
     productName.value,
     productDiscription.value,
     productPrice.value,
-    productDiscount.value
+    productDiscount.value,
+    productCategory.value,
+    user.value.uid,
+    userData.value.userName
   );
 };
 </script>
@@ -33,12 +63,17 @@ const add = async () => {
   >
     <div class="m-2">
       <p class="text-sm font-semibold py-2">Add Products</p>
-      <form action="" class="flex flex-wrap gap-2">
+      <form action="" @submit.prevent="add" class="flex flex-wrap gap-2">
         <div class="border p-2">
           <label for="name" class="block py-1 text-xs font-medium text-gray-700"
             >Product Name:</label
           >
-          <input type="text" v-model="productName" class="border p-1" />
+          <input
+            required
+            type="text"
+            v-model="productName"
+            class="border p-1"
+          />
         </div>
         <div class="border p-2">
           <label for="name" class="block py-1 text-xs font-medium text-gray-700"
@@ -50,13 +85,37 @@ const add = async () => {
           <label for="name" class="block py-1 text-xs font-medium text-gray-700"
             >Product Price:</label
           >
-          <input type="number" v-model="productPrice" class="border p-1" />
+          <input
+            required
+            type="number"
+            v-model="productPrice"
+            class="border p-1"
+          />
         </div>
         <div class="border p-2">
           <label for="name" class="block py-1 text-xs font-medium text-gray-700"
             >Product Discount:</label
           >
           <input type="number" v-model="productDiscount" class="border p-1" />
+        </div>
+        <div class="border p-2">
+          <label for="name" class="block py-1 text-xs font-medium text-gray-700"
+            >Product Category:</label
+          >
+          <select
+            name=""
+            required
+            v-model="productCategory"
+            id=""
+            class="border text-sm p-1"
+          >
+            <option value="Electronics" class="text-xs font-semibold">
+              Electronics
+            </option>
+            <option value="Kitchen" class="text-xs font-semibold">
+              Kitchen Tools
+            </option>
+          </select>
         </div>
       </form>
       <div class="flex justify-start items-center gap-3">
