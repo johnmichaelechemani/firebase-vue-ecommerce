a40a16b018e44571f15222976496234f31790409
Your Name <deudajm@gmail.com>
2024-12-25 10:03:31 +0800

    `Update NotFound.vue template and script setup with new styles and router link`

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,24 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
   <div
-    class="sm:ml-72 ml-20 fixed top-14 left-0 sm:w-[calc(100%-18rem)] w-[calc(100%-5rem)] h-full"
+    class="w-screen h-screen flex justify-center items-center text-lg font-semibold"
   >
-    <div class="flex justify-center items-center text-lg font-semibold h-[calc(100%-5rem)]">
-      Hey! I can't find what you're looking.
+    <div>
+      <div class="">Hey! I can't find what you're looking.</div>
+      <RouterLink to="/" class="flex items-center gap-2 text-sm my-5">
+        <div class="border p-2 shadow">
+          <Icon
+            icon="material-symbols-light:arrow-back"
+            width="30"
+            height="30"
+          />
+        </div>
+        Go Back Human
+      </RouterLink>
     </div>
   </div>
 </template>
