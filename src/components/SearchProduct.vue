a1c19ee3363d39f626ed4202396c957142c21235
Your Name <deudajm@gmail.com>
2025-01-07 19:48:29 +0800

    `Refactor SearchProduct.vue: add input field and search results`

--- src/components/SearchProduct.vue
+++ src/components/SearchProduct.vue
@@ -2,17 +2,58 @@
   <div>
     <div class="fixed inset-0 z-50 bg-gray-800/30 backdrop-blur">
       <div class="relative flex justify-center items-center">
-        <div
-          class="border border-gray-700/50 bg-gray-100 py-1.5 px-1 ml-2 my-5 text-sm w-96 flex"
-        >
-          <div class="px-1 flex justify-center items-center">
-            <Icon icon="material-symbols-light:search" width="24" height="24" />
+        <div class="mx-2 sm:m-0">
+          <div
+            class="border w-full border-gray-700/50 bg-gray-100 py-1.5 px-1 mt-5 mb-3 text-sm sm:w-96 flex"
+          >
+            <div class="px-1 flex justify-center items-center">
+              <Icon
+                icon="material-symbols-light:search"
+                width="24"
+                height="24"
+              />
+            </div>
+            <input
+              type="text"
+              class="w-full outline-none bg-transparent py-1 placeholder:text-sm placeholder:text-gray-700"
+              placeholder="Search.."
+            />
           </div>
-          <input
-            type="text"
-            class="w-full outline-none bg-transparent py-1 placeholder:text-sm placeholder:text-gray-700"
-            placeholder="Search.."
-          />
+          <div
+            class="sm:max-w-96 w-full p-2 border backdrop-blur-md bg-gray-500/10 max-h-80"
+          >
+            <div class="pb-5">
+              <p class="text-sm font-semibold text-gray-100">
+                Search Discovery
+              </p>
+              <div class="flex flex-wrap gap-2 my-2">
+                <span
+                  class="flex text-xs border bg-gray-200/90 py-1 px-2 font-semibold justify-start items-center"
+                >
+                  <Icon icon="noto:fire" width="20" height="20" />
+                  <span class="text-gray-800">Trouser For Men</span>
+                </span>
+                <span
+                  class="flex text-xs border bg-gray-200/90 py-1 px-2 font-semibold justify-start items-center"
+                >
+                  <Icon icon="noto:fire" width="20" height="20" />
+                  <span class="text-gray-800">Fabric Men Pants</span>
+                </span>
+                <span
+                  class="flex text-xs border bg-gray-200/90 py-1 px-2 font-semibold justify-start items-center"
+                >
+                  <Icon icon="noto:fire" width="20" height="20" />
+                  <span class="text-gray-800">Computer</span>
+                </span>
+                <span
+                  class="flex text-xs border bg-gray-200/90 py-1 px-2 font-semibold justify-start items-center"
+                >
+                  <Icon icon="noto:fire" width="20" height="20" />
+                  <span class="text-gray-800">Cellphone</span>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
