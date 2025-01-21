37ab70f5c0868d30047fdaab55e858d6ddb80920
Your Name <deudajm@gmail.com>
2025-01-21 15:03:00 +0800

    `Added status filtering to mallOrders.vue`

--- src/views/MallsViews/mallOrders.vue
+++ src/views/MallsViews/mallOrders.vue
@@ -1,4 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const status = ref([
+  {
+    name: "All",
+  },
+  {
+    name: "Pending",
+  },
+  {
+    name: "Processing",
+  },
+  {
+    name: "Shipped",
+  },
+  {
+    name: "Delivered",
+  },
+  {
+    name: "Cancelled",
+  },
+  {
+    name: "Refunded",
+  },
+  {
+    name: "Returned",
+  },
+  {
+    name: "Completed",
+  },
+]);
+
+const selectedStatus = ref("All");
+const selected = (name) => {
+  selectedStatus.value = name;
+  console.log(name);
+};
+</script>
 
 <template>
   <div
@@ -8,6 +46,22 @@
       class="m-2 overflow-y-scroll no-scrollbar h-[calc(100vh-3.5rem)] pb-32"
     >
       <p class="text-sm font-semibold py-2">Order Management</p>
+      <div class="mb-2 border w-full overflow-x-scroll no-scrollbar p-2">
+        <div class="flex gap-3">
+          <button
+            @click="selected(i.name)"
+            v-for="i in status"
+            :class="
+              selectedStatus === i.name
+                ? 'text-gray-900 border border-b-2 border-b-gray-800 shadow-sm py-1 px-2'
+                : 'text-gray-500'
+            "
+            class="text-sm font-medium"
+          >
+            {{ i.name }}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
